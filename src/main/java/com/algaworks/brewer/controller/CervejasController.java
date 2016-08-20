2c69743df2f7188774d02f3b9e5556a43e8d38fd
Adilson <asu2u@yahoo.com.br>
2016-08-20 20:53:14 -0300

    Prototipando cadastro de cerveja com Bootstrap

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
 
@@ -14,16 +16,20 @@ import com.algaworks.brewer.model.Cerveja;
 public class CervejasController {
 
 	@RequestMapping("novo")
-	public String novo(){
+	public String novo(Cerveja cerveja){
 		return "cerveja/CadastroCerveja";
 	}
 	
 	@RequestMapping( value = "novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result){
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model,
+				RedirectAttributes attributes){
 		if(result.hasErrors()){
-			System.out.println("Erro na validação");
+			return novo(cerveja);
 		}
 		System.out.println(cerveja.getSku());
-		return "cerveja/CadastroCerveja";
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		return "redirect:novo";
 	}
+	
+		
 }
--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,13 +1,22 @@
 package com.algaworks.brewer.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 
-	@NotBlank
+	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	@Size(min = 5,  max = 25, message = "Nome deve conter entre 5 a 25 caracteres")
 	private String nome;
-
+	
+	@Size(min = 1, max = 150, message = "Descrição dever conter de 1 a 150 caracteres")
+	private String descricao;
+	
+	
 	public String getSku() {
 		return sku;
 	}
@@ -22,6 +31,14 @@ public class Cerveja {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 }
