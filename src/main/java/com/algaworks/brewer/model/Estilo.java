ef6858c60c49794b2f490408d0c05b2741cd2583
Adilson <asu2u@yahoo.com.br>
2016-10-06 00:35:23 -0300

    Implementando o cadastro do Estilo, validacao e persistir no banco.

--- src/main/java/com/algaworks/brewer/model/Estilo.java
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Table(name = "estilo")
@@ -19,7 +22,9 @@ public class Estilo implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
-		
+	
+	@NotBlank(message = "O nome é Obrigatorio")
+	@Size(min = 5, max = 15, message = "O nome deve conter entre 5 a 15 caracteres.")
 	private String nome;
 	
 	@OneToMany(mappedBy = "estilo")
