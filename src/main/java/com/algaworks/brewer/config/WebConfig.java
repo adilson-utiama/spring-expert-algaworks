94e239861a88ddeb57053085f72a0c05cd89d004
Adilson <asu2u@yahoo.com.br>
2016-10-05 23:27:30 -0300

    Convertendo valores numericos

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -1,17 +1,23 @@
 package com.algaworks.brewer.config;
 
+import java.math.BigDecimal;
+import java.util.Locale;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
@@ -54,15 +60,8 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		engine.addDialect(new LayoutDialect());
 		return engine;
 	}
-	
-	@Bean
-	public FormattingConversionService mvcConversionService(){
-		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
-		conversionService.addConverter(new EstiloConverter());
-		return conversionService;
-	}
-	
-	
+		
+		
 	private ITemplateResolver templateResolver(){
 		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
 		resolver.setApplicationContext(applicationContext);
@@ -76,5 +75,27 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
 	}
+	
+	//Responsavel por fazer conversoes de valores e outros
+	@Bean
+	public FormattingConversionService mvcConversionService(){
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		conversionService.addConverter(new EstiloConverter());
+		
+		NumberStyleFormatter bigDecimalFormatter = new NumberStyleFormatter("#,##0.00");
+		conversionService.addFormatterForFieldType(BigDecimal.class, bigDecimalFormatter);
+		
+		NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
+		conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
+		
+		return conversionService;
+	}
+	
+	//Forca a usar o pt-BR independente dos navegadores
+	@Bean
+	public LocaleResolver localeResolver(){
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
 	
 }
