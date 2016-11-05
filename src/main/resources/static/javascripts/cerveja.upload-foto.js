23d360c71c1caed085aec9b5218bdae8b9dfa49b
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-11-05 18:07:43 -0200

    Refatorando script do upload da foto

---
+++ src/main/resources/static/javascripts/cerveja.upload-foto.js
@@ -0,0 +1,55 @@
+var Brewer = Brewer || {};
+
+Brewer.UploadFoto = (function(){
+	
+	function UploadFoto(){
+		this.inputNomeFoto = $('input[name=foto]');
+		this.inputContentType = $('input[name=contentType]');
+		
+		this.htmlFotoCervejaTemplate = $('#foto-cerveja').html();
+		this.template = Handlebars.compile(this.htmlFotoCervejaTemplate);
+		//var htmlFotoCerveja = template({ nomeFoto: resposta.nome});
+		this.containerFotoCerveja = $('.js-container-foto-cerveja');
+		this.uploadDrop = $('#upload-drop');
+	}
+	
+	UpploadFoto.prototype.iniciar = function(){
+		var settings = {
+			type: 'json',
+			filelimit: 1,
+			allow: '*.(jpg|jpeg|png)',
+			action: this.containerFotoCerveja.data('url-fotos'),
+			complete: onUploadCompleto.bind(this)
+		}
+		
+		UIkit.uploadSelect($('#upload-select'), settings);
+		UIkit.uploadDrop($('#upload-drop'), settings);
+	}
+	
+	function onUploadCompleto(resposta){
+		this.inputNomeFoto.val(resposta.nome);
+		this.inputContentType.val(resposta.contentType);
+		 
+		this.uploadDrop.addClass('hidden');
+		var htmlFotoCerveja = this.template({ nomeFoto : resposta.nome});
+		this.containerFotoCerveja.append(htmlFotoCerveja);
+		
+		$('.js-remove-foto').on('click', onRemoverFoto.bind(this));
+	}
+	
+	function onRemoverFoto() {
+		$('.js-foto-cerveja').remove();
+		uploadDrop.removeClass('hidden');
+		inputNomeFoto.val('');
+		inputContentType.val('');
+	}
+	
+	
+	return UpploadFoto;
+	
+})();
+
+$(function(){
+	var uploadFoto = new Brewer.UploadFoto();
+	uploadFoto.iniciar();
+});
