6cea502c1fbb42fd4717593d9e8db626585d17aa
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-11-07 23:54:33 -0200

    Redimensionando a imagem - salvando o thumbnail - e EventListener

--- src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -14,6 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.algaworks.brewer.storage.FotoStorage;
 
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.name.Rename;
+
+
+
 public class FotoStorageLocal implements FotoStorage{
 
 	private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
@@ -56,6 +61,32 @@ public class FotoStorageLocal implements FotoStorage{
 		}
 	}
 	
+	@Override
+	public void salvar(String foto) {
+		try {
+			Files.move(this.localTemporario.resolve(foto), this.local.resolve(foto));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro movendo a foto para destino final.", e);
+		}
+		
+		try {
+			Thumbnails.of(this.local.resolve(foto).toString()).size(40, 68).toFiles(Rename.PREFIX_DOT_THUMBNAIL);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao gerar o thumbnail da foto.", e);
+		}
+		
+	}
+	
+	@Override
+	public byte[] recuperar(String nome) {
+		try {
+			return Files.readAllBytes(this.local.resolve(nome));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao ler a foto", e);
+		}
+	}
+
+	
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local);
@@ -80,6 +111,9 @@ public class FotoStorageLocal implements FotoStorage{
 		}
 		return novoNome;
 	}
+
+	
+	
 
 	
 }
