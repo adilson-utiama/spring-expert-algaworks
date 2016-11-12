eb022da4fa9f9121a62cc9319e32b3c71d132825
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-11-12 21:10:23 -0200

    Desafio - Pesquisa de Estilos

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -1,13 +1,17 @@
 package com.algaworks.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +19,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.exception.NomeEstiloJaCadastradoException;
+import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.repository.Estilos;
+import com.algaworks.brewer.repository.filter.EstiloFilter;
 import com.algaworks.brewer.service.CadastroEstiloService;
 
 @Controller
@@ -24,7 +32,10 @@ import com.algaworks.brewer.service.CadastroEstiloService;
 public class EstilosController {
 	
 	@Autowired
-	private CadastroEstiloService cadastroEsiloService;
+	private CadastroEstiloService cadastroEstiloService;
+	
+	@Autowired
+	private Estilos estilos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo(Estilo estilo){
@@ -40,7 +51,7 @@ public class EstilosController {
 		}
 		
 		try{
-			cadastroEsiloService.salvar(estilo);
+			cadastroEstiloService.salvar(estilo);
 		}catch(NomeEstiloJaCadastradoException e){
 			result.rejectValue("nome", e.getMessage(), e.getMessage());
 			return novo(estilo);
@@ -63,12 +74,23 @@ public class EstilosController {
 		//ControllerAdvice tratando a exception, aqui não ha necessidade de tratar a exception
 		//o Spring ja faz o tratamento,neste caso so funciona com retorno de um ResponseEntity
 		//try{
-			estilo = cadastroEsiloService.salvar(estilo);
+			estilo = cadastroEstiloService.salvar(estilo);
 //		}catch(NomeEstiloJaCadastradoException e){
 //			return ResponseEntity.badRequest().body(e.getMessage());
 //		}
 		
 		return ResponseEntity.ok(estilo);
 		
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar(EstiloFilter estiloFilter, BindingResult result, 
+			@PageableDefault(size = 2) Pageable pageable, HttpServletRequest httpServletRequest ){
+		ModelAndView mv = new ModelAndView("estilos/PesquisaEstilos");
+		
+		PageWrapper<Estilo> paginaWrapper = new PageWrapper<Estilo>(estilos.filtrar(estiloFilter, pageable), httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+				
+		return mv;
 	}
 }
