da9432a5a80781f22deef7e046328f25c158ddd7
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-11-13 20:37:47 -0200

    Desafio - Configurando mascara do CEP com JQuery

---
+++ src/main/resources/static/javascripts/cliente.mascara-cep.js
@@ -0,0 +1,20 @@
+var Brewer = Brewer || {};
+
+Brewer.MascaraCep = (function() {
+	
+	function MascaraCep() {
+		this.inputCep = $('#cep');
+	}
+	
+	MascaraCep.prototype.iniciar = function(){
+		this.inputCep.mask('00000-000');
+	}
+
+	return MascaraCep;
+	
+}());
+
+$(function() {
+	var mascaraCep = new Brewer.MascaraCep();
+	mascaraCep.iniciar();
+});
