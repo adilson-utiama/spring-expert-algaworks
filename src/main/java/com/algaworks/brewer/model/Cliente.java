653c36b0b4c5a6d49251c961dd5900d1d3da26d1
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-11-15 14:56:49 -0200

    Aplicando migração de Estado e Cidade e implementação do combobox de estado

---
+++ src/main/java/com/algaworks/brewer/model/Cliente.java
@@ -0,0 +1,103 @@
+package com.algaworks.brewer.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long codigo;
+	private String nome;
+	private String documentoReceitaFederal;
+	private String telefone;
+	private String email;
+	private TipoPessoa tipoPessoa;
+	private List<Endereco> enderecos;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDocumentoReceitaFederal() {
+		return documentoReceitaFederal;
+	}
+
+	public void setDocumentoReceitaFederal(String documentoReceitaFederal) {
+		this.documentoReceitaFederal = documentoReceitaFederal;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public TipoPessoa getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(TipoPessoa tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/algaworks/brewer/model/Endereco.java
@@ -0,0 +1,10 @@
+package com.algaworks.brewer.model;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+
+}
