b6c626eec85995ea77026d088537b0f812487284
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-11-26 19:46:08 -0200

    Criando as paginas 404 e 500

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -49,10 +49,11 @@ public class CervejasController {
 	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model,
 				RedirectAttributes attributes){
+		
 		if(result.hasErrors()){
 			return novo(cerveja);
 		}
-		
+				
 		cadastroCervejaService.salvar(cerveja);
 		
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
