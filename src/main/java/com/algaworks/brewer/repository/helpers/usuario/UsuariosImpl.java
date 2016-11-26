b190c87f82fd9ac2021b8bf1540d9ba8062c0fe9
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-11-25 22:57:31 -0200

    Carregando as permissões do usuário

--- src/main/java/com/algaworks/brewer/repository/helpers/usuario/UsuariosImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helpers/usuario/UsuariosImpl.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository.helpers.usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
@@ -21,6 +22,12 @@ public class UsuariosImpl implements UsuariosQueries{
 					.getResultList()
 					.stream()
 					.findFirst();
+	}
+
+	@Override
+	public List<String> permissoes(Usuario usuario) {
+		String query = "select distinct p.nome from Usuario u inner join u.grupos g inner join g.permissoes p where u = :usuario";
+		return manager.createQuery(query, String.class).setParameter("usuario", usuario).getResultList();
 	}
 
 }
--- src/main/java/com/algaworks/brewer/repository/helpers/usuario/UsuariosQueries.java
+++ src/main/java/com/algaworks/brewer/repository/helpers/usuario/UsuariosQueries.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository.helpers.usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.algaworks.brewer.model.Usuario;
@@ -7,4 +8,5 @@ import com.algaworks.brewer.model.Usuario;
 public interface UsuariosQueries {
 
 	public Optional<Usuario> porEmailEAtivo(String email);
+	public List<String> permissoes(Usuario usuario);
 }
