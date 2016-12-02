4c45dd6907c8c521e186d5a79cb9987bab2d4da6
Adilson <Adilson@WIN-P5FUK94HTUF.localdomain>
2016-12-01 23:59:11 -0200

    Testando a tabela de itens de venda com JUnit

---
+++ src/test/java/comr/algaworks/brewer/venda/TabelaItensVendaTest.java
@@ -0,0 +1,61 @@
+package comr.algaworks.brewer.venda;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.venda.TabelaItensVenda;
+
+public class TabelaItensVendaTest {
+
+	private TabelaItensVenda tabelaItensVenda;
+	
+	@Before
+	public void setUp(){
+		this.tabelaItensVenda = new TabelaItensVenda();
+	}
+	
+	@Test
+	public void deveCalcularValorTotalSemItens() throws Exception {
+		assertEquals(BigDecimal.ZERO, tabelaItensVenda.getValorTotal());
+	}
+	
+	@Test
+	public void deveCalcularValorTotalComUmItem() throws Exception {
+		Cerveja cerveja = new Cerveja();
+		BigDecimal valor = new BigDecimal("8.90");
+		cerveja.setValor(valor);
+		
+		tabelaItensVenda.adicionar(cerveja, 1);
+		
+		assertEquals(valor, tabelaItensVenda.getValorTotal());
+	}
+	
+	@Test
+	public void deveCalcularValorTotalComVariosItens() throws Exception {
+		Cerveja c1 = new Cerveja();
+		BigDecimal v1 = new BigDecimal("8.90");
+		c1.setValor(v1);
+		
+		Cerveja c2 = new Cerveja();
+		BigDecimal v2 = new BigDecimal("4.99");
+		c2.setValor(v2);
+		
+		tabelaItensVenda.adicionar(c1, 1);
+		tabelaItensVenda.adicionar(c2, 2);
+		
+		
+		assertEquals(new BigDecimal("18.88"), tabelaItensVenda.getValorTotal());
+		
+	}
+	
+	
+	
+	
+	
+	
+}
